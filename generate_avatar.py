6cb1e513c7172b52c2b33a608890eba1f52297d2
Michael Zats <92814061+Michaelzats@users.noreply.github.com>
2022-06-09 18:52:33 +0200

    Add files via upload

---
+++ generate_avatar.py
@@ -0,0 +1,11 @@
+from avatar_generator import AvatarGenerator
+
+
+def generate_avatar():
+    generator = AvatarGenerator(
+        "/Users/mihailzac/Library/Mobile Documents/com~apple~CloudDocs/pixel-punk-avatars-main/avatar_generator.py")
+    generator.generate_avatar(100)
+
+
+if __name__ == "__main__":
+    generate_avatar()
